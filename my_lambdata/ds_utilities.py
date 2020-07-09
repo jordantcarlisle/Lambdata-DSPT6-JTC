04b72f8ce4c9b1748987e837d77f35b74259838e
jordantcarlisle <jordantcarlisle@gmail.com>
2020-07-08 21:13:50 -0500

    proof of concept from lecture

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -2,5 +2,6 @@ def enlarge(n):
     '''This function will multiply the input by 100'''
     return n * 100
 
-y = int(input('Choose a number:'))
-print(y, enlarg(y))
+if __name__ == '__main__':
+    y = int(input('Choose a number:'))
+    print(y, enlarge(y))
