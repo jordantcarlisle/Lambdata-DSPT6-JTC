0690b54160855eddc9a0b1177b3462382c63db2d
jordantcarlisle <jordantcarlisle@gmail.com>
2020-07-08 22:52:46 -0500

    created download url and list to dataframe utility functions for the my_lambdata package

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -5,3 +5,17 @@ def enlarge(n):
 if __name__ == '__main__':
     y = int(input('Choose a number:'))
     print(y, enlarge(y))
+
+def download(url):
+    filename = url.split('/')[-1]
+    print(f'Downloading {url}')
+    r = requests.get(url)
+    with open(filename, 'wb') as f:
+        f.write(r.content)
+    print(f'Downloaded {filename}')
+
+
+def to_dataframe_column(list):
+    x = pd.series(list)
+    df = DataFram(x)
+    print(df)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Lambdata-DSPT6-JTC", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Jordan Carlisle",
     author_email="jordantcarlisle@gmail.com",
     description="A small example package",
