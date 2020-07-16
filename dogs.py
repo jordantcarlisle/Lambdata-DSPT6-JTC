a4232871e7ea459979d5d1415d97eb07b07af2e5
jordantcarlisle <jordantcarlisle@gmail.com>
2020-07-15 21:06:01 -0500

    cleaned up defects in functions

--- dogs.py
+++ dogs.py
@@ -2,30 +2,23 @@ from pdb import set_trace as breakpoint
 
 
 class Dog():
-
-    """Use the Dog class to store and retrieve information about dogs. name,
-    age, housebroke, breed."""
-
-    def __init__(self, name, age, housebroke, breed):
-        self.housebroke = housebroke
+    def __init__(self, name, age, housebroke):
         self.name = name
         self.age = age
-        self.breed = breed
         self.housebroke = housebroke
 
     def is_housebroke(self):
         if self.housebroke == True:
             print(f'{self.name} is housebroken!')
         else:
-            print(f'{self.name} is not housebroken...')
+            print(f'{self.name} is not housebroken... :(')
 
-    #    super(, self).__init__()
-    #    self.arg = arg
 
 class Beagle(Dog):
-    pass
+    def barks_alot(self, barker)
+
 
 if __name__ == "__main__":
 
-    lucky = Dog('Lucky', 2, False, True)
+    lucky = Dog('Lucky', 2, True)
     breakpoint()
