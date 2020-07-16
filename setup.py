aa51bf7c57f3b88b108d1a87d698cd9cf339a44f
jordantcarlisle <jordantcarlisle@gmail.com>
2020-07-15 21:18:52 -0500

    added testing and notebooks

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Lambdata-DSPT6-JTC2",  # Replace with your own username
+    name="Lambdata-DSPT6-JTC",  # Replace with your own username
     version="0.0.4",
     author="Jordan Carlisle",
     author_email="jordantcarlisle@gmail.com",
-    description="A small example package",
+    description="This is a small test package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jordantcarlisle/Lambdata-DSPT6-JTC",
